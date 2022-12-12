cd334be83532ea3169a7eba7aacd646b88ee08b6
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2022-12-12 06:14:36 +0100

    Updated my portfolio design

--- public/portfolio/custom/custom.js
+++ public/portfolio/custom/custom.js
@@ -117,50 +117,8 @@ $(function() {
     typeWriter();
 });
 
-function formSubmit(path) {
-    $('#formid').attr("action", path);
-    $('#formid').submit();
-}
-
+//Print
 function printHTML() {
     $('#sidebarCollapse').click();
     setTimeout(function(){ window.print(); }, 1500);
-}
-
-function validateForm() {
-  var name =  document.getElementById('name').value;
-  if (name == "") {
-      document.querySelector('.status').innerHTML = "Name cannot be empty";
-      return false;
-  }
-  var email =  document.getElementById('email').value;
-  if (email == "") {
-      document.querySelector('.status').innerHTML = "Email cannot be empty";
-      return false;
-  } else {
-      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if(!re.test(email)){
-          document.querySelector('.status').innerHTML = "Email format invalid";
-          return false;
-      }
-  }
-  var subject =  document.getElementById('subject').value;
-  if (subject == "") {
-      document.querySelector('.status').innerHTML = "Subject cannot be empty";
-      return false;
-  }
-  var body =  document.getElementById('message').value;
-  if (body == "") {
-      document.querySelector('.status').innerHTML = "Message cannot be empty";
-      return false;
-  }
-  Email.send({
-        SecureToken : "2b089397-5448-4e2d-975a-61d2a07feab1",
-        To : 'michael.franiatte@gmail.com',
-        From : email,
-        Subject : subject,
-        Body : name + ": </br>" + body
-  }).then(
-      message => alert('mail sent successfully!')
-  );
 }
